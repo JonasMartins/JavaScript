1ed9e5950082ade743a3e034c91031045a3c5948
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-12 15:24:29 -0300

    tcndc 3.3.2

--- Complete-NodeJS/03/node-notes/app.js
+++ Complete-NodeJS/03/node-notes/app.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
+var res = notes.addNote();
+//console.log(res);
+
+console.log('Result: ', notes.add(17,-5));
+
+/*
 
 var user = os.userInfo();
-console.log(user);
-/*
-fs.appendFile('greetings.txt', 'HelloWorld', function (err) {
+//console.log(user);
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old!` , function (err) {
  
   if(err){
     console.log('Unable to read file');
