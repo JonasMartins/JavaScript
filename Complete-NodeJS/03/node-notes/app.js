976927107bc13b870510c6261085780fe0a44529
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-15 19:48:09 -0300

    tcndc 3.4

--- Complete-NodeJS/03/node-notes/app.js
+++ Complete-NodeJS/03/node-notes/app.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
 const os = require('os');
+
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var res = notes.addNote();
+/* function from nodash */
+console.log(_.isString('test'));
+
+
+/* function from nodash */
+var filterArray = _.uniq(['test', 1,'test',2,1,3,4,5]);
+console.log(filterArray);
+
+/*Saida: ['test',1,2,3,4,5], retirando as duplicatas */
+
+
+//var res = notes.addNote();
 //console.log(res);
 
-console.log('Result: ', notes.add(17,-5));
+//console.log('Result: ', notes.add(17,-5));
 
 /*
 
