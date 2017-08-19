1b91e3ace15a1eefa1743ce5e3200a9ee2b9e7c9
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-19 20:30:03 -0300

    tcrwapdc 022

--- Complete-React-Web/HelloReact/public/components/Greeter.js
+++
@@ -1,6 +0,0 @@
-function greeter () {
-  document.write('From greeter function');
-}
-
-
-module.exports = greeter;
--- Complete-React-Web/HelloReact/webpack.config.js
+++ Complete-React-Web/HelloReact/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* onde começar a processar o codigo */
-  entry: './public/app.js',
+  entry: './public/app.jsx',
   /* output */
   output: {
     /* dirname é o path para a pasta atual */
@@ -9,6 +9,33 @@ module.exports = {
   },
   /* Um array de extenções que queremos processar */
   resolve: {
-    extensions: [' ','.js','.jsx']
+    extensions: [' ', '.js', '.jsx']
+  },
+
+  module: {
+    /**
+     * [loaders description]
+     * @type {Array}
+     *
+     *  Resumo desse processo:
+     *
+     *  Usamos o babel-loader,
+     *  a através de react, processe-os e depois
+     *  use a saida para o es2015, é sabido o tipo de
+     *  arquivo a ser usado pela extensão test que
+     *  maneja todos os arquivos com as extensões
+     *  especificadas pela forma de regular expressions.
+     *
+     *  Abaixo de teste temos as pastas em que não queremos
+     *  que o babel-loader processe, converta
+     */
+    loaders: [{
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }]
   }
 };
