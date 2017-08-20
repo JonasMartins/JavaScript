fe7c87d252a50073e5ee33c8f40e7ef5fa5c08be
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-20 20:33:06 -0300

    fswawmr 013

--- Meteor/score-keep/client/main.js
+++ Meteor/score-keep/client/main.js
@@ -9,12 +9,21 @@ import {
  * @return {[type]}           [description]
  *
  * app vem do id do arquivo main.html
- * 
+ * Só pode haver um root element, ou seja apenas um div, 
+ * podem haver vários outros elemetnos desse div root
  */
 Meteor.startup(function() {
-
+  let title = 'Account Seetings';
   let name = 'Andrew';
-  let jsx = <p>Hello {name}!</p>;
+  let jsx = (
+
+    {/* comments */}
+    <h1>{title}</h1>
+    <div>
+      <p>Hello {name}!</p>
+    </div> 
+
+    );
 
   ReactDOM.render(jsx, document.getElementById('app'));
 });
