7145559beff3d5305165122bf70963778cfc8fd5
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-20 20:58:01 -0300

    fswawmr 014

--- Meteor/score-keep/client/main.js
+++ Meteor/score-keep/client/main.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import {
   Meteor
 } from 'meteor/meteor';
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RUN: METEOR RUN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+const players = [
+  {
+    _id: '1',
+    name: 'Lauren',
+    score: '99',
+  },
+  
+  {
+    _id: '2',
+    name: 'Cory',
+    score: '-1',
+  },
+
+  {
+    _id: '3',
+    name: 'Andrew',
+    score: '-12',
+  }
+  
+  ];
+
+const renderPlayers = function(playersList){
+
+  return playersList.map(function(player){
+  
+    return <p key={player._id}>{ player.name } has {player.score} !</p>
+  
+  }); 
+
+};
+
 /**
  * [startup description]
  * @param  {[type]} funcrion( [description]
@@ -16,13 +49,15 @@ Meteor.startup(function() {
   let title = 'Account Seetings';
   let name = 'Andrew';
   let jsx = (
-
-    {/* comments */}
-    <h1>{title}</h1>
+    
     <div>
+      <h1>{title}</h1>
       <p>Hello {name}!</p>
-    </div> 
-
+    
+      {renderPlayers(players)}
+      
+    </div>     
+    
     );
 
   ReactDOM.render(jsx, document.getElementById('app'));
