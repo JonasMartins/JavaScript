9a97358af50b0386590432e1c1a56ab81ffe6b13
JonasMartins <jonas.s.martins@hotmail.com>
2017-08-24 21:22:23 -0300

    tcndc 4.7

--- Complete-NodeJS/weather-app/app.js
+++ Complete-NodeJS/weather-app/app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+}).help().alias('help', 'h').argv;
+
+/* argv.address é a entrada do usuario no terminal e essa função vai codificar o 20% nos espaços */
+var encodedAddress = encodeURIComponent(argv.address)
+
+/*Ex entrada: 1614 south broad street philadelphia*/
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  //console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
