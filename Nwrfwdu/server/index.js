2a063ebbed279c48be7a511f564b87ac5f92d8a6
JonasMartins <jonas.s.martins@hotmail.com>
2017-10-08 21:43:39 -0300

    nwrfwdu 028

--- Nwrfwdu/server/index.js
+++ Nwrfwdu/server/index.js
@@ -1,53 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
+require('./services/passport');
 
 const app = express();
 
+// ao inves de criar uma variavel espcifica para passar app
+// como argumento:  
+// const authRoutes = require('./routes/authRoutes');
+// authRoutes(app);
 
-
-
-passport.use(
-
-  new GoogleStrategy({
-
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token',accessToken);
-    console.log('refresh token',refreshToken);
-    console.log('profile',profile);
-  })
-
-);
-
-app.get('/auth/google', 
-  
-  // objeto passaport tentando autenticar esse usuario
-  // que veio desse link atravéz da estratégia google 
-  // essa string google refere-se a GoogleStrategy mais acima
-  
-  passport.authenticate('google', {
-    scope: ['profile','email']
-  })
-);
-//  a diferença dessa chamada a estratégia google
-//  é que dessa vez o code já vem junto na url e o 
-//  passport vai saber manejar esse novo dado para 
-//  trocar code pelo user profile.
-//  após clicar na autenticação da conta, o google
-//  vai redirecionar de volta para o server da nossa aplicação
-//  
-//  e retorna junto com ele e loga o accesstoken que é passado
-//  com callback vindo do google 
-//  
-app.get('/auth/google/callback', 
-  passport.authenticate('google') );
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
